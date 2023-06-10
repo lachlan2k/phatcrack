2f7bdac98ac97371d6e5c1290e36830d3356a990
Lachlan <lachlan@lachlandavidson.com>
2023-06-10 17:45:59 +1200

    Add linting to codegen script

--- ts_types_codegen.sh
+++ ts_types_codegen.sh
@@ -7,3 +7,6 @@ cd common
 tscriptify -package=github.com/lachlan2k/phatcrack/common/pkg/apitypes -target=../frontend/src/api/types.ts -interface ./pkg/apitypes/*
 
 sed -i 's/key: int/key: number/' ../frontend/src/api/types.ts
+
+cd ../frontend
+npm run lint
