4bf4e8d8812db3f0afc107e723952cdab7b124a2
Lachlan <lachlan@lachlandavidson.com>
2023-08-16 13:01:38 +1200

    Look for jobs that should be running, but aren't, and then fail them. Also ensure we mark agents as disconnected when the API starts

--- api/internal/fleet/state_reconcilition.go
+++ api/internal/fleet/state_reconcilition.go
@@ -25,6 +25,8 @@ func stateReconciliation() error {
 		return err
 	}
 
+	agentMap := make(map[string]*db.Agent, 0)
+
 	// Jobs that we deem to have "failed" because the agent is dead
 	jobsFailed := make([]string, 0)
 	jobsOk := make(map[string]interface{}, 0) // Using like a set
@@ -33,6 +35,8 @@ func stateReconciliation() error {
 		info := agent.AgentInfo.Data
 		activeJobs := agent.AgentInfo.Data.ActiveJobIDs
 		needsSave := false
+
+		agentMap[agent.ID.String()] = &agent
 
 		newInfo := info
 
@@ -74,33 +78,68 @@ func stateReconciliation() error {
 		}
 	}
 
-	pendingJobs, err := db.GetAllPendingJobs(true)
+	incompleteJobs, err := db.GetAllIncompleteJobs(true)
 	if err != nil {
 		return err
 	}
 
-	for _, job := range pendingJobs {
-		if job.RuntimeData.Status == db.JobStatusAwaitingStart {
-			if time.Since(job.RuntimeData.StartRequestTime) > acceptableJobStartTime {
-				// todo set it failed
-				_, jobOk := jobsOk[job.ID.String()]
-				if jobOk {
-					log.Printf("Warn: Job was marked as awaiaitng-start in database, but an agent was found to be running it. This probably indicates a race condition, and we'll let it slide for now.")
-					db.SetJobStarted(job.ID.String(), "Unknown", time.Now())
-				} else {
-					db.SetJobExited(job.ID.String(), db.JobStopReasonTimeout, "The job did not start in time", time.Now())
+	for _, job := range incompleteJobs {
 
-					// Tell agent to kill this job, incase it *is* running but it just didn't make it through.
-					// It's an unlikely error condition, but just probably tidy to do
-					agentConnection, ok := fleet[job.AssignedAgent.ID.String()]
-					if ok {
-						agentConnection.sendMessage(wstypes.JobKillType, wstypes.JobKillDTO{
-							JobID: job.ID.String(),
-						})
+		switch job.RuntimeData.Status {
+		case db.JobStatusAwaitingStart:
+			{
+				if time.Since(job.RuntimeData.StartRequestTime) > acceptableJobStartTime {
+					// todo set it failed
+					_, jobOk := jobsOk[job.ID.String()]
+					if jobOk {
+						log.Printf("Warn: Job was marked as awaiaitng-start in database, but an agent was found to be running it. This probably indicates a race condition, and we'll let it slide for now.")
+						db.SetJobStarted(job.ID.String(), "Unknown", time.Now())
+					} else {
+						db.SetJobExited(job.ID.String(), db.JobStopReasonTimeout, "The job did not start in time", time.Now())
+
+						// Tell agent to kill this job, incase it *is* running but it just didn't make it through.
+						// It's an unlikely error condition, but just probably tidy to do
+						agentConnection, ok := fleet[job.AssignedAgent.ID.String()]
+						if ok {
+							agentConnection.sendMessage(wstypes.JobKillType, wstypes.JobKillDTO{
+								JobID: job.ID.String(),
+							})
+						}
+					}
+				}
+			}
+
+		case db.JobStatusCreated, db.JobStatusStarted:
+			{
+				jobId := job.ID.String()
+				_, isJobOk := jobsOk[jobId]
+				// No non-dead agent is running the job. So the agent might have died
+				if !isJobOk {
+					if job.AssignedAgentID == nil {
+						continue
+					}
+
+					agentThatShouldBeRunningJob, isAgentOk := agentMap[job.AssignedAgentID.String()]
+					if !isAgentOk {
+						continue
+					}
+
+					isJobInList := false
+					for _, jobRunningOnAgent := range agentThatShouldBeRunningJob.AgentInfo.Data.ActiveJobIDs {
+						if jobRunningOnAgent == jobId {
+							isJobInList = true
+							break
+						}
+					}
+
+					// We expected the agent to be running the job, but it wasn't
+					if !isJobInList {
+						db.SetJobExited(jobId, db.JobStopReasonFailed, "The job unexpectedly disappeared from the agent's list of running jobs", time.Now())
 					}
 				}
 			}
 		}
+
 	}
 
 	for _, jobId := range jobsFailed {
@@ -136,7 +175,24 @@ func QueueStateReconciliation() {
 }
 
 func Setup() error {
-	err := stateReconciliation()
+	agents, err := db.GetAllAgents()
+	if err != nil {
+		return nil
+	}
+
+	// this is a bit manual and could be achieved in one UPDATE query, but I think this is fine for now
+	for _, agent := range agents {
+		if agent.AgentInfo.Data.Status == db.AgentStatusDead {
+			continue
+		}
+		err = db.UpdateAgentStatus(agent.ID.String(), db.AgentStatusUnhealthyAndDisconnected)
+		if err != nil {
+			return err
+		}
+	}
+
+	// This state re-conciliation we manually invoke will go out and mark any agents as dead and jobs as failed, as necessar1y
+	err = stateReconciliation()
 	if err != nil {
 		return fmt.Errorf("failed to perform initial state reconciliation: %v", err)
 	}
