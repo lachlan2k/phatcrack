18d28a6dbd1cc146b6b12f265b1c7e2e2e2af87a
Lachlan <lachlan@lachlandavidson.com>
2023-08-19 08:09:54 +1200

    Get ready for work on sharding attacks

---
+++ api/internal/attacksharder/attacksharder.go
@@ -0,0 +1,7 @@
+package attacksharder
+
+import "github.com/lachlan2k/phatcrack/api/internal/db"
+
+func ShardAttackIntoJobs(attack *db.Attack, numAgents int) ([]db.Job, error) {
+	return nil, nil
+}
