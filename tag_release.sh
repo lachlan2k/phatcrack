870812015e6991ac4121dbc523ce8ae45de32bf8
Lachlan <lachlan@lachlandavidson.com>
2023-09-02 10:50:50 +1200

    chore: Add release script

---
+++ tag_release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+RELEASE_VERSION=$1
+
+sed -i "s@lachlan2k/phatcrack/releases/download/.*/@lachlan2k/phatcrack/releases/download/${RELEASE_VERSION}/@" README.md
+
+git commit -am "chore: Version ${RELEASE_VERSION}"
+
+git push
+git tag ${RELEASE_VERSION}
+git push --tags
