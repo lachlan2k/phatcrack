e20d19248a025e890645ffd965fba03494acd597
Lachlan <lachlan@lachlandavidson.com>
2023-09-02 21:38:46 +1200

    ci: Print version string when building agent

--- agent/build.sh
+++ agent/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-CGO_ENABLED=0 go build -ldflags="-X github.com/lachlan2k/phatcrack/agent/internal/version.version=$(git describe --tags)" -o phatcrack-agent main.go
+VERSION_STR=$(git describe --tags)
+
+echo "Building version ${VERSION_STR}"
+
+CGO_ENABLED=0 go build -ldflags="-X github.com/lachlan2k/phatcrack/agent/internal/version.version=${VERSION_STR}" -o phatcrack-agent main.go
