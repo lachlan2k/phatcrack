861550e2901c53693707ed704440b158f915cf8a
Lachlan <lachlan@lachlandavidson.com>
2023-09-02 21:52:17 +1200

    ci: Don't shallow fetch, to ensure version tag can be described

--- api/build.sh
+++ api/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-go build -ldflags="-X github.com/lachlan2k/phatcrack/api/internal/version.version=$(git describe --tags)" -o phatcrack-api main.go
+VERSION_STR=$(git describe --tags)
+
+echo "Building version ${VERSION_STR}"
+
+go build -ldflags="-X github.com/lachlan2k/phatcrack/api/internal/version.version=${VERSION_STR}" -o phatcrack-api main.go
