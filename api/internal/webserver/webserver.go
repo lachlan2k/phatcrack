9277f4bdb9659d83f47d7c604fa0c465b405db45
Lachlan <lachlan@lachlandavidson.com>
2023-09-10 00:57:20 +1200

    fix: Extend session lifetime

--- api/internal/webserver/webserver.go
+++ api/internal/webserver/webserver.go
@@ -14,7 +14,7 @@ import (
 
 func makeSessionHandler() auth.SessionHandler {
 	return &auth.InMemorySessionHandler{
-		SessionTimeout:     15 * time.Minute,
+		SessionTimeout:     30 * time.Minute,
 		SessionMaxLifetime: 4 * time.Hour,
 	}
 }
