6754990cc4909d27bf91ac30ddacaf3c90ab2c25
Lachlan <lachlan@lachlandavidson.com>
2024-03-05 17:09:39 +1300

    fix: Prevent duplicate hashes to potfile by adding a unique constraint and okay-to-fail on insert

--- api/internal/db/potfile.go
+++ api/internal/db/potfile.go
@@ -1,12 +1,18 @@
 package db
 
+import (
+	"gorm.io/gorm/clause"
+)
+
 type PotfileEntry struct {
 	UUIDBaseModel
-	Hash         string
-	PlaintextHex string
-	HashType     uint
+	Hash         string `gorm:"index:idx_uniq,unique"`
+	PlaintextHex string `gorm:"index:idx_uniq,unique"`
+	HashType     uint   `gorm:"index:idx_uniq,unique"`
 }
 
-func AddPotfileEntry(entry *PotfileEntry) (*PotfileEntry, error) {
-	return entry, GetInstance().Create(entry).Error
+func AddPotfileEntry(newEntry *PotfileEntry) (*PotfileEntry, error) {
+	return newEntry, GetInstance().
+		Clauses(clause.OnConflict{DoNothing: true}).
+		Create(newEntry).Error
 }
