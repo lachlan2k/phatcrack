fc8858cde3b9446690818044acfd35eab578c2bd
Lachlan <lachlan@lachlandavidson.com>
2024-03-05 23:46:05 +1300

    chore: Lint

--- frontend/src/api/potfile.ts
+++ frontend/src/api/potfile.ts
@@ -2,7 +2,9 @@ import { client } from '.'
 import type { PotfileSearchResponseDTO } from './types'
 
 export function searchPotfile(hashes: string[]): Promise<PotfileSearchResponseDTO> {
-    return client.post('/api/v1/potfile/search', {
-        hashes
-    }).then(res => res.data)
+  return client
+    .post('/api/v1/potfile/search', {
+      hashes
+    })
+    .then((res) => res.data)
 }
