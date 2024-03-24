eae0d392eda7ed8c52628a11e6c9c5bc935db1f6
Lachlan <lachlan@lachlandavidson.com>
2024-03-24 22:48:29 +1300

    ci(e2e): tidy up playwright config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -11,32 +11,21 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  workers: 1,
 
   reporter: [
-    'html',
+    ['html'],
     [process.env.CI ? 'github' : 'list']
   ],
 
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     baseURL: process.env.E2E_BASE_URL || 'https://localhost:8443',
     ignoreHTTPSErrors: true
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'initial.setup',
@@ -55,12 +44,5 @@ export default defineConfig({
       // testMatch: /.*\/spec\.ts/,
       dependencies: ['auth.setup']
     },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  ]
 });
