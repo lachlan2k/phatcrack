9e28b672bf5e1002cab93fc134f5478a0c0d507f
Lachlan <lachlan@lachlandavidson.com>
2024-09-01 16:47:48 +1200

    feat: Show datetimes on hover for timeSince displays

---
+++ frontend/src/components/TimeSinceDisplay.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { timeSince } from '@/util/units'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  timestamp: number
+}>()
+
+const timestampDisplayed = computed(() => new Date(props.timestamp).toLocaleDateString(undefined, { hour: '2-digit', minute: '2-digit' }))
+</script>
+
+<template>
+  <div class="tooltip" :data-tip="timestampDisplayed">
+    <span>{{ timeSince(props.timestamp) }}</span>
+  </div>
+</template>
