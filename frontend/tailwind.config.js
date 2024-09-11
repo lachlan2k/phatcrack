3c0625371d1d17edee0aa9fac12e6bc2d5ebc519
Lachlan <lachlan@lachlandavidson.com>
2024-09-11 22:28:20 +1200

    deps: Fix broken prettier tailwind plugin

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui'), require('@tailwindcss/typography'), require('prettier-plugin-tailwindcss')],
+  plugins: [require('daisyui'), require('@tailwindcss/typography')],
   daisyui: {
     themes: [{
       corporate: {
