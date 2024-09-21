81f711ef0d5b009c153f7a7d91badc1e75ff0ced
Lachlan <lachlan@lachlandavidson.com>
2024-09-21 14:44:15 +1200

    feat: Show hint of mask effective length

--- frontend/src/components/Wizard/MaskInput.vue
+++ frontend/src/components/Wizard/MaskInput.vue
@@ -9,6 +9,13 @@ const props = defineProps<{
 
 const emit = defineEmits(['update:modelValue'])
 
+const effectiveLength = computed(() => {
+  const mask = props.modelValue
+  const withDummies = mask.replace(/\?[a-zA-Z]/g, 'z')
+
+  return withDummies.length
+})
+
 const value = computed({
   get: () => props.modelValue,
   set: (value: string) => emit('update:modelValue', value)
@@ -16,8 +23,9 @@ const value = computed({
 </script>
 
 <template>
-  <label class="label font-bold">Select Mask</label>
+  <label class="label pl-0 font-bold">Enter Mask</label>
   <input type="text" placeholder="Mask" v-model="value" class="input input-bordered w-full max-w-xs" />
+  <p class="text-xs mt-1">Length: {{effectiveLength}}</p>
   <div class="mt-4">
     <span
       class="tooltip tooltip-bottom"
