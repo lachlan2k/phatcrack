ed748439dd0960a0f0e4b373540786930746bd88
Lachlan <lachlan@lachlandavidson.com>
2024-09-22 15:51:18 +1200

    feat: Add catch-all frontend error handler

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import Toast from 'vue-toastification'
+import Toast, { useToast } from 'vue-toastification'
 
 import 'vue-toastification/dist/index.css'
 
@@ -20,5 +20,17 @@ app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(Toast, {})
 app.use(router)
+
+const toast = useToast()
+app.config.errorHandler = (err) => {    
+    if (err instanceof Error) {
+        toast.error('Unexpected frontend error:\n\n' + err.message)
+    } else if (err?.toString != null) {
+        toast.error('Unexpected frontend error:\n\n' + err.toString())
+    } else {
+        toast.error('Unexpected frontend error. Check console for details.')
+    }
+    console.error(err)
+}
 
 app.mount('#app')
